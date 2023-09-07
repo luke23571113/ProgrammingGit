c98bb528b37e3fcf2828c663ae87f902528d99af
Luke Collins <luke23571113@gmail.com>
2023-09-07 08:53:52 -0700

    implemented blob class

---
+++ Blob.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+import java.security.*;
+
+
+public class Blob {
+
+    //nb: do not add multiple entries
+
+    public Blob (String path)
+    {
+        try
+        {
+            //make the objects folder if it doesn't exist
+            File objects = new File ("./objects");
+            objects.mkdirs();
+
+            //read in the file contents
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while (br.ready())
+            {
+                sb.append ((char) br.read());
+            }
+            br.close();
+
+            String hash = getHashString(sb.toString());
+            File newFile = new File ("./objects/" + hash);
+            newFile.createNewFile();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getHashString (String input)    
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            md.update(input.getBytes());
+            byte[] b = md.digest();
+
+            char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+            StringBuffer buf = new StringBuffer();
+            for (int j=0; j<b.length; j++) {
+            buf.append(hexDigit[(b[j] >> 4) & 0x0f]);
+            buf.append(hexDigit[b[j] & 0x0f]);
+            }
+            return buf.toString();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            return ""; 
+        }
+    }
+}
---
+++ GitTester.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class GitTester
+{
+    public static void main (String[] args)
+    {
+        Blob blob = new Blob ("example.txt");
+
+        System.out.println("finished program");
+    }
+}
